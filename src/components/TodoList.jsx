3529693cde47fb252429bac6b0ecf36d5409c0cc
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-25 22:56:54 -0400

    added edit function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,9 +7,21 @@ export default function TodoList( {loading, itemList, setItemList, setLoading}) 
         const response = await fetch(`http://127.0.0.1:5002/items/${id}`, {
             method: "DELETE",
         })
+    }
 
-        setLoading(false)
-
+    const handleUpdate = async (task) => {
+        setLoading(true)
+        const modifiedItem = {
+            done: !task.done
+        }
+        const response = await fetch(`http://127.0.0.1:5002/items/${task.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(modifiedItem)
+        })
+        
     }
     useEffect(() => {
         fetch('http://127.0.0.1:5002/items')
@@ -18,6 +30,7 @@ export default function TodoList( {loading, itemList, setItemList, setLoading}) 
         .catch(alert)
         .finally(() => setLoading(false))
     }, [itemList])
+
     return (
     <section>
         <List
@@ -26,7 +39,7 @@ export default function TodoList( {loading, itemList, setItemList, setLoading}) 
          loading={loading}
          size = 'large'
          renderItem={(task) => (
-            <List.Item className={(task.done) && 'done'} actions = {[<a className = "actions">edit</a>, <a className = "actions" onClick={() => handleDelete(task.id)}>delete</a>]}>
+            <List.Item className={(task.done) && 'done'} actions = {[<a className = "actions" onClick={() => handleUpdate(task)}>status</a>, <a className = "actions" onClick={() => handleDelete(task.id)}>delete</a>]}>
                 {task.item}
             </List.Item>
          )}
