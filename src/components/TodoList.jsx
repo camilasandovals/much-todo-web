8c0496ede726b7f623fb86d68b4688d67310996b
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-25 22:00:34 -0400

    added the delete function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,15 @@ import { useEffect } from 'react'
 import { List } from 'antd'
 export default function TodoList( {loading, itemList, setItemList, setLoading}) {
 
-  
+    const handleDelete = async (id) => {
+        setLoading(true)
+        const response = await fetch(`http://127.0.0.1:5002/items/${id}`, {
+            method: "DELETE",
+        })
+
+        setLoading(false)
+
+    }
     useEffect(() => {
         fetch('http://127.0.0.1:5002/items')
         .then(resp => resp.json())
@@ -18,7 +26,7 @@ export default function TodoList( {loading, itemList, setItemList, setLoading}) 
          loading={loading}
          size = 'large'
          renderItem={(task) => (
-            <List.Item className={(task.done) && 'done'} actions = {[<a className = "actions">edit</a>, <a className = "actions" onClick={handleDelete}>delete</a>]}>
+            <List.Item className={(task.done) && 'done'} actions = {[<a className = "actions">edit</a>, <a className = "actions" onClick={() => handleDelete(task.id)}>delete</a>]}>
                 {task.item}
             </List.Item>
          )}
